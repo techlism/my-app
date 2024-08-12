fda20346f49332f130b1d28997179feff2bf0916
techlism <2021itb049.kundan@students.iiests.ac.in>
2024-08-12 11:42:36 +0530

    creating template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { AccordionDemo } from "./Accordion";
+import { CardWithForm } from "./Card";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center max-w-7xl mx-auto border p-4 rounded-lg space-y-5">
+      <h1 className="text-lg font-bold">App</h1>
+      <AccordionDemo />
+      <CardWithForm />
     </div>
   );
 }
-
-export default App;
